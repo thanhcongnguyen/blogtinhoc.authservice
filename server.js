24eead567794faa50cfe3af27cdace7dcd845d7d
thanhcongnguyen <nguyenthanhcong@unimarmores.com>
2018-08-08 02:11:10 +0700

    edit server.js

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 3000;
+
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Comming soon!');
+  res.send('Comming soon!\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, function(){
+  console.log(`server running with port ${PORT}`)
+});
